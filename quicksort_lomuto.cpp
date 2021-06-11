b14da1f1b233fa13259d96f905f733b343b3ba94
isabel <60983383+preciousakura@users.noreply.github.com>
2021-06-11 00:52:28 -0300

    Update quicksort_lomuto.cpp

--- quicksort_lomuto.cpp
+++ quicksort_lomuto.cpp
@@ -5,33 +5,35 @@ using std::swap;
 
 template <typename T>
 int particionar (T *v, int i, int p, int f) {
-  int left = i;
-  for(int j = i; j <= f; j++) {
-    if(v[j] <= v[p] && j != p) {
-        if(p == left && p+1 < f) swap(v[p], v[p+1]);
+
+  int left = i + 1;
+  swap(v[p], v[i]);
+  p = i;
+  for(int j = left; j <= f; j++) {
+    if(v[j] < v[p]) {
         swap(v[j], v[left]);
         left++;
     } 
-  }  swap(v[p],v[left]); 
-  return p; 
+  } swap(v[p], v[left-1]); 
+  return left-1; 
 }
 
 template <typename T>
-void quickSort (T *v, int n) {
-  if(n <= 0) return;
-  int pivot = particionar(v, 0, n/2, n);
-  quickSort(v, pivot);
-  pivot = particionar(v, pivot, (pivot+n)/2, n);
-  quickSort(v, pivot);
+void quickSort(T *a, int low, int high)
+{
+	if (low < high) {
+		int index_pivot = particionar(a, low, (low+high)/2, high);
+		quickSort(a, low, index_pivot - 1);
+		quickSort(a, index_pivot + 1, high);	
+	}
 }
 
 int main() {
-  int v[] = {2,1,2,1,2,100,2,1,2,1,2,1,2,1,2};
-  quickSort(v, (sizeof(v)/sizeof(v[0]))-1);
+  int v[] = {0,1,0,10,1,0,1,0,1,0,1,0,10,10,500};
+  quickSort(v, 0, sizeof(v)/sizeof(v[0]) - 1);
 
   for(int i = 0; i < sizeof(v)/sizeof(v[0]); i++) {
     cout << v[i] << ' ';
   }
   return 0;
 }
-

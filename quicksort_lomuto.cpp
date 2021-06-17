bdd343bb573e3af283967b56e042da9447650a54
isabel <60983383+preciousakura@users.noreply.github.com>
2021-06-16 22:24:01 -0300

    Update quicksort_lomuto.cpp

--- quicksort_lomuto.cpp
+++ quicksort_lomuto.cpp
@@ -1,11 +1,5 @@
-#include <iostream>
-#include <algorithm>
-using std::cout;
-using std::swap;
-
 template <typename T>
-int particionar (T *v, int i, int p, int f) {
-
+int particionar_l (T *v, int i, int p, int f) {
   int left = i + 1;
   swap(v[p], v[i]);
   p = i;
@@ -19,21 +13,11 @@ int particionar (T *v, int i, int p, int f) {
 }
 
 template <typename T>
-void quickSort(T *a, int low, int high)
+void quicksort_l(T *a, int low, int high)
 {
 	if (low < high) {
-		int index_pivot = particionar(a, low, (low+high)/2, high);
-		quickSort(a, low, index_pivot - 1);
-		quickSort(a, index_pivot + 1, high);	
+		int index_pivot = particionar_l(a, low, pivo(low, high), high);
+		quicksort_l(a, low, index_pivot - 1);
+		quicksort_l(a, index_pivot + 1, high);	
 	}
-}
-
-int main() {
-  int v[] = {0,1,0,10,1,0,1,0,1,0,1,0,10,10,500};
-  quickSort(v, 0, sizeof(v)/sizeof(v[0]) - 1);
-
-  for(int i = 0; i < sizeof(v)/sizeof(v[0]); i++) {
-    cout << v[i] << ' ';
-  }
-  return 0;
 }

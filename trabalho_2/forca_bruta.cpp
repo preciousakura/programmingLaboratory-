e327ecf04ca6786659c507289844e967b653299d
isabel <60983383+preciousakura@users.noreply.github.com>
2021-07-15 00:52:40 -0300

    Delete forca_bruta.cpp

--- trabalho_2/forca_bruta.cpp
+++
@@ -1,7 +0,0 @@
-void forca_bruta(const char* P, const char* T, int *O) {
-  for(const char* i = T; *i != '\0'; ++i) {
-    const char *j = i, *k = P;
-    while(*k != '\0' && *j == *k) { ++j; ++k; }
-    if(*k == '\0') { *O = i - T; ++O;}
-  } *O = -1;
-}

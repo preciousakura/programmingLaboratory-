75555d62fd006f066c6f0ec8a52dd2781601d138
isabel <60983383+preciousakura@users.noreply.github.com>
2021-07-15 00:51:17 -0300

    Update kmp.cpp

--- trabalho_2/kmp.cpp
+++ trabalho_2/kmp.cpp
@@ -1,7 +1,3 @@
-int count_str(const char* s, unsigned int size) { // contar números de caracteres
-  if(s[size] == '\0') return size;
-  return count_str(s, ++size);
-}
 
 void prefix(const char* P, int* A, int size) {
   int i = 1, j = 0; 
@@ -21,20 +17,26 @@ void prefix(const char* P, int* A, int size) {
 bool kmp(const char *P, const char *T, int *O) {
   int size_p = count_str(P, 0); // tamanho do padrão
   int *A = new int[size_p]; 
+
   if(A != nullptr) { 
     prefix(P, A, size_p); // computa o prefixo  
     const char *i = T, *j = P;
-    while (*i != '\0') {
-      while (*j != '\0' && *i == *j){ ++i; ++j; }
-      if(*j == '\0') {
-        *O = (i - T) - (j - P);
-        ++O;
+
+    while(*i != '\0') {
+      if(*j == *i) { ++i; ++j; }
+
+      if(*j == '\0') { // padrão encontrado
+        *O = (i - T) - (j - P); ++O;
+        j = j = P + A[(j - P) - 1]; 
       }
-      if(j - P == 0) ++i;
-      else j = P + A[(j - P) - 1];
+
+      else {
+        if(*j != *i) {
+          if(j - P != 0) j = j = P + A[(j - P) - 1];
+          else ++i;
+        }
+      }
     }
-    delete [] A; 
-    *O = -1; 
-    return true;
+    delete []A; *O = -1; return true;
   } else return false;
 }
